16cee5d7b78b89ca29601a6d436ca6e993967306
Ronan Murphy <ronan.murphy@originmarkets.com>
2019-02-26 22:38:14 +0000

    API Hello world.

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello World")
+	routes := gin.Default()
+    routes.GET("/", func(c *gin.Context) {
+        c.JSON(200, gin.H{"message": "Hello World"})
+    })
+    routes.Run()
 }
